56440dab2adacf85a887fa09724462d625b6479c
schoesche <georg.achermann@mobi.ch>
2016-02-02 10:45:19 +0100

    clean up

--- app/components/linklist/linklist.js
+++ app/components/linklist/linklist.js
@@ -9,25 +9,25 @@ angular.module('fulgurator.linksModule',[
         var vmLinksCtrl = this;
 
         vmLinksCtrl.rowCollection = {};
-
         vmLinksCtrl.getRowCollectionDataService = dataService.getRowCollection();
 
+        /**
         console.log("getRowCollectionDataService1:" + (vmLinksCtrl.getRowCollectionDataService == {}));
         console.log("getRowCollectionDataService2:" + (vmLinksCtrl.getRowCollectionDataService == []));
         console.log("getRowCollectionDataService:" + vmLinksCtrl.getRowCollectionDataService);
         console.log("linksModuleData.allLinks():" + dataService.loadDefaultLinks());
+        */
 
         vmLinksCtrl.loadLinkFile = function () {
-
             vmLinksCtrl.rowCollection = dataService.loadDefaultLinks();
-
         };
 
         vmLinksCtrl.loadLinkFile();
 
+        /**
         console.log("getRowCollectionDataService loadLinkFile:" + vmLinksCtrl.rowCollection[0].groupname);
         console.log("getRowCollectionDataService loadLinkFile:" + vmLinksCtrl.rowCollection[1].groupname);
-
+        */
     }]);
 
 
